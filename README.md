29de84f0f0dfffb85e4e8d347db157af1739bc39
Utsab Dahal <70103872+meutsabdahal@users.noreply.github.com>
2022-11-06 16:46:42 +0545

    working with numbers

--- README.md
+++ README.md
@@ -5,3 +5,4 @@ This project is bunch of python files which consists of python basic examples.
 - hello_world.py: prints hello world in the terminal  
 - draw_shape.py: draws a triangle shape    
 - string.py: working with strings
+- number.py: working with numbers
---
+++ number.py
@@ -0,0 +1,41 @@
+from math import *
+
+# printing a number
+print(555.555)
+
+# performing basic arithmetic
+print(1.2 + 200)
+print(500 - 268)
+print(200 * 2)
+print(500 / 2)
+print(500 % 2)  # displays the remainder of 500/2
+print((5 * 25) - (52 - 25))
+
+# converting a number into string
+my_num = -25
+print(str(my_num) + ' is now a string')
+
+# using build-in function
+# getting an absolute value of a number
+print(abs(my_num))
+
+# calculating power of a number
+print(pow(3, 3))
+
+# returns a larger number between two passed number
+print(max(22, 300))
+
+# returns a smaller number between two passed number
+print(min((5, 6)))
+
+# rounds a number
+print(round(9.99))
+
+# chops up the decimal value
+print(floor(9.99))
+
+# rounds a number up
+print(ceil(9.49))
+
+# returns a square root of a number
+print(sqrt(49))
