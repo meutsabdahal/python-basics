ba28cab373eb93552090b99f47ddd620eec85759
Utsab Dahal <70103872+meutsabdahal@users.noreply.github.com>
2022-11-06 16:00:03 +0545

    working with strings

--- README.md
+++ README.md
@@ -2,5 +2,6 @@
 
 This project is bunch of python files which consists of python basic examples.
 
-- hello_world.py: Prints hello world in the terminal  
-- draw_shape.py: Draws a triangle shape    
+- hello_world.py: prints hello world in the terminal  
+- draw_shape.py: draws a triangle shape    
+- string.py: working with strings
---
+++ string.py
@@ -0,0 +1,37 @@
+# printing simple string
+print('Python String')
+
+# printing the above string in new line
+print('Python\nString')
+
+# printing quotation in a string
+print('Python \'String')
+
+# concatenation
+string_value = 'Python'
+print(string_value + ' String')
+
+# using build-in function
+# converting string to lower case
+upper_case = 'PYTHON STRING'
+print(upper_case.lower())
+
+# converting string to lower case
+lower_case = 'python string'
+print(lower_case.upper())
+
+# checking whether the string is lower case or upper case
+print(lower_case.islower())
+print(upper_case.islower())
+
+# finding out the length of string
+print(len(string_value))
+
+# getting specific character from the string
+print(string_value[3])
+
+# finding where specific character from the string is
+print(string_value.index('y'))
+
+# replacing the values/character from string
+print(string_value.replace('ython', 'hone'))
